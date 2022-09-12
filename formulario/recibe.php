cee0bf5ee93c3a9b5f84a9347a77ea2b3b5ebb73
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-12 05:51:05 -0300

    Add files via upload

--- formulario/recibe.php
+++ formulario/recibe.php
@@ -1,8 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 <?php
+ echo $_POST["name"].'<br />'; 
 
-$name = $_POST["nombre"];
-echo $name;
+ echo $_POST['sexo'].'<br />';
 
-?>
+ echo print_r($_POST).'<br />';
+ 
+ ?>
+ <br>
 
-
+  
+</body>
+</html>
