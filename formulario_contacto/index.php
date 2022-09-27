5130312a8392ddbbed3a5b39791986532e69aacb
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-27 18:47:32 -0300

    Add files via upload

--- formulario_contacto/index.php
+++ formulario_contacto/index.php
@@ -7,6 +7,7 @@ if (isset($_POST['submit'])) {
   $nombre = $_POST['nombre'];
   $correo = $_POST['correo'];
   $mensaje = $_POST['mensaje'];
+  $telefono = $_POST['telefono'];
 
   //$algo = $_POST[["nombre", "correo", "mensaje"]];
   // print_r($_POST);
@@ -27,6 +28,38 @@ if (isset($_POST['submit'])) {
   } else {
     $errores .= 'Por favor, ingresa un correo';
   } 
+
+  if (!empty($telefono)) {
+    $telefono = htmlspecialchars($telefono);
+    $telefono = trim($telefono);
+    $telefono = stripcslashes($telefono);
+
+    if (strlen($telefono) !== 9) {
+      $errores .= 'Por favor, ingresa un teléfono válido';
+    }
+  }
+
+  if (!empty($mensaje)) {
+    $mensaje = htmlspecialchars($mensaje);
+    $mensaje = trim($mensaje);
+    $mensaje = stripcslashes($mensaje);
+  } else {
+    $errores .= 'Por favor, ingresa un mensaje';
+  }
+
+  if (!$errores) {
+    $enviar_a = 'tunombre@empresa.com'; 
+    $asunto = 'Mensaje Automático desde MiSitio.com';
+    $mensaje_preparado = 'De: '.$nombre.'<br />';
+    $mensaje_preparado .= 'Correo: '.$correo.'<br />';
+    $mensaje_preparado .= 'Mensaje: '.$mensaje.'<br />';
+
+    //mail($enviar_a, $asunto, $mensaje_preparado);
+    $enviado = 'true';
+
+  }  
+
+
 }
 
 require 'index.view.php';
