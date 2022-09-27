2ff8d54c219e76f396823c8bc1aaefe65e43a4fa
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-27 17:34:19 -0300

    Add files via upload

--- formulario_contacto/index.view.php
+++ formulario_contacto/index.view.php
@@ -5,13 +5,25 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="estilos.css">
+  <link href="https://fonts.googleapis.com/css2?family=Nanum+Brush+Script&family=Nunito:ital,wght@0,200;0,400;0,500;0,700;1,300;1,400&family=Yuji+Boku&display=swap" rel="stylesheet">
   <title>Formulario Contacto</title>
 </head>
 <body>
-  <h1>Chupi doo do</h1>
   <div class="wrap">
-    <form action="<?php echo htmlspecialchars($_SERVER[PHP_SELF]); ?>" method="post">
-    <input type="text" class="form-control" id="nombre" name="nombre" placeholder="nombre" value=''>
+    <h1>Olá, Brasil</h1>
+    <form 
+      action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" 
+      method="post"
+    >
+    
+    <input 
+      type="text" 
+      class="form-control" 
+      id="nombre" 
+      name="nombre" 
+      placeholder="nombre" 
+      value=""
+    >
     
     <input 
       type="text"
@@ -23,11 +35,42 @@
     >
 
     <textarea 
-      name="Mensaje" 
+      name="mensaje" 
       class='form-control' 
       id='mensaje' 
       placeholder='mensaje'
     ></textarea>
+
+    <!-- 
+      estos alertas se implementarán con PHP
+
+    <div class="alert error">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    </div>
+
+    <div class="alert succes">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    </div>
+
+    -->
+    <?php if (!empty($errores)):  ?>
+      <div class="alert error">
+        <?php echo $errores; ?>
+      </div>
+    <?php elseif ($enviado): ?>
+      <div class="alert succes">
+        <p>Enviado Correctamene</p>
+      </div>
+    <?php endif ?>
+
+    <input 
+      type="submit" 
+        name='submit' 
+        class="btn btn-primary" 
+        value="Enviar"
+    >
+
+    
 
     </form>
   </div>
