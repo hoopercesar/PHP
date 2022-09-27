f97f8b95100e9bc758c349afaad88e041130c9a1
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-09-27 17:33:52 -0300

    Add files via upload

--- formulario_contacto/index.php
+++ formulario_contacto/index.php
@@ -1,9 +1,34 @@
 <?php
 
+$errores = '';
+$enviado = '';
+
+if (isset($_POST['submit'])) {
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  $mensaje = $_POST['mensaje'];
+
+  //$algo = $_POST[["nombre", "correo", "mensaje"]];
+  // print_r($_POST);
+ 
+  if (!empty($nombre)) {
+    // trim() quita espacio al inicio y final de string
+    $nombre = trim($nombre);
+    $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);  
+  } else {
+    $errores .= 'Por favor, ingresa un nombre'.'<br />';
+  }
+
+  if (!empty($correo)) {
+    $correo = filter_var($correo, FILTER_SANITIZE_EMAIL); 
+    if (!filter_var($correo, FILTER_VALIDATE_EMAIL)) {
+      $errores .= 'Por favor, ingresa un correo Válido';
+    } 
+  } else {
+    $errores .= 'Por favor, ingresa un correo';
+  } 
+}
+
 require 'index.view.php';
 
 ?>
-
-
-  
-
