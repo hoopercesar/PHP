e2d7b346c126feb498503e4a899a78add049d4b7
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-03 08:12:09 -0300

    Add files via upload

--- paginacion/index.view.php
+++ paginacion/index.view.php
@@ -1,3 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="estilos.css">
+  <title>Paginación</title>
+</head>
+<body>
+  <div class="contenedor">
+    <h1>Articulos</h1>
+    <section class="articulos">
+      <ul>
+        <li>1.- Lorem ipsum dolor sit amet consectetur</li>
+        <li>1.- Lorem ipsum dolor sit amet consectetur</li>
+        <li>1.- Lorem ipsum dolor sit amet consectetur</li>
+        <li>1.- Lorem ipsum dolor sit amet consectetur</li>
+        <li>1.- Lorem ipsum dolor sit amet consectetur</li>
+      </ul>
+    </section>
 
-?>
+    <section class="paginacion">
+      <ul>
+        <li class="disabled"><a href="#">&laquo;</a></li>
+        <li class="active"><a href="#">1</a></li>
+        <li><a href="#">2</a></li>
+        <li><a href="#">3</a></li>
+        <li><a href="#">4</a></li>
+        <li><a href="#">&raquo;</a></li>
+        
+      </ul>
+    </section>
+  </div>
+  
+</body>
+</html>
