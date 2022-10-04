2cba4efc49ccdb36dd663fdf609ba0b6ab7929ce
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-04 18:17:16 -0300

    Add files via upload

--- login_registro/views/contenido.view.php
+++ login_registro/views/contenido.view.php
@@ -3,13 +3,51 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <script 
+    type="module" 
+    src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+    ></script>
+
+  <script 
+    nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    ></script>
+
+  <link 
+    href="https://fonts.googleapis.com/css2?family=Raleway:400,300" 
+    rel="stylesheet" 
+    type="text/css"
+  >
+
+
   <meta name="viewport" content="width=device-width, user-scalable=no,
     initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-  <link rel="stylesheet" href="estilos.css">
+  <link rel="stylesheet" href="css/estilos.css">
   <title>Contenido</title>
 </head>
 <body>
+  <div class="contenedor">
+    <h1 class="titulo">Contenido del Sitio 
+    <ion-icon name="analytics"></ion-icon>
+    </h1>
+    <a href="cerrar.php">Cerrar Sesion</a>
+    <div class="contenido">
+    <ion-icon name="heart"></ion-icon>
+      <article>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+          Perferendis asperiores nemo corporis nam accusamus voluptas 
+          vel praesentium animi deserunt natus rerum neque in obcaecati, 
+          eos cum perspiciatis qui illo sequi?</p>
+          
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Perferendis cupiditate odit iste ea modi autem obcaecati 
+          perspiciatis explicabo necessitatibus. Aut quia dicta perferendis, 
+          reprehenderit beatae tempora error delectus voluptates ad!</p>
+      </article>
+    </div>
+
+    
+  </div>
   
 </body>
 </html>
