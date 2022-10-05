68777bc0ab7f3625863a6b73c0b890296bcef6eb
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-05 07:26:57 -0300

    Add files via upload

---
+++ login_registro/views/registrate.view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+  <script 
+    type="module" 
+    src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+  ></script>
+
+  <script 
+    nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+  ></script>
+
+  <link 
+    href="https://fonts.googleapis.com/css2?family=Raleway:400,300" 
+    rel="stylesheet" 
+    type="text/css"
+  >
+
+
+  <meta name="viewport" content="width=device-width, user-scalable=no,
+    initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+  <link rel="stylesheet" href="css/estilos.css">
+  <title>Registrate</title>
+</head>
+<body>
+  <div class="contenedor">
+    <h1 class="titulo">Regístrate
+    <ion-icon name="analytics"></ion-icon>
+    <ion-icon name="rocket"></ion-icon>
+    <ion-icon name="heart"></ion-icon>
+    <ion-icon name="business"></ion-icon>
+    </h1>
+    <hr class="border">
+    
+    </div>
+
+    
+  </div>
+  
+</body>
+</html>
