79f8c8cf6ecdf46995ddfc83a341cd4c1436100d
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-06 07:53:48 -0300

    Add files via upload

--- login_registro/views/registrate.view.php
+++ login_registro/views/registrate.view.php
@@ -4,15 +4,14 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-
   <script 
     type="module" 
     src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
   ></script>
 
   <script 
-    nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+    nomodule 
+    src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
   ></script>
 
   <link 
@@ -31,14 +30,57 @@
 <body>
   <div class="contenedor">
     <h1 class="titulo">Regístrate
-    <ion-icon name="analytics"></ion-icon>
+    
     <ion-icon name="rocket"></ion-icon>
-    <ion-icon name="heart"></ion-icon>
-    <ion-icon name="business"></ion-icon>
     </h1>
     <hr class="border">
-    
-    </div>
+
+    <form 
+      action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"
+      method="POST"
+      class='formulario'
+      name='login'      
+      >
+      <div class="form-group">
+        <ion-icon name="person-sharp">
+        </ion-icon>
+        <input type="text" name="usuario" class="usuario" placeholder='Usuario'>
+      </div>
+
+      <div class="form-group">
+        <ion-icon name="lock-closed-sharp">
+        </ion-icon>
+        <input type="password" name="password" class="password" placeholder='Password'>
+      </div>
+
+      <div class="form-group">
+        <ion-icon name="lock-closed-sharp">
+        </ion-icon>
+        <input 
+          type="password" 
+          name="password2" 
+          class="password_btn" 
+          placeholder='Repitir Password'
+        >
+        <ion-icon 
+          name="arrow-forward-circle-sharp"
+          onclick="login.submit()"
+        ></ion-icon>
+      </div>
+
+      <?php if(!empty($errores)): ?>
+        <div class="errores">
+          <ul>
+            <?php echo $errores; ?>
+          </ul>
+        </div>
+      <?php endif; ?>
+
+    </form>
+    <p class="texto-registrate">
+      Si ya estás registrado
+      <a href="login.php">Inicia Sesión</a>
+    </p>
 
     
   </div>
