72eb6c753f5da964d50652a5c7022070c10d2742
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-07 06:26:19 -0300

    Add files via upload

--- login_registro/login.php
+++ login_registro/login.php
@@ -1,5 +1,14 @@
 <?php
 
+// primero se comprueba si el usuario ya ha iniciado sesión.
+// para éso se usa el isset de $_SESION['usuario], si existe
+// lo enviamos al index.php que lo redirigirá a contenido.php
+if (isset($_SESION['usuario'])){
+  header('Location: index.php')
+}
+
+
+
 require 'views/login.view.php';
 
 ?>
