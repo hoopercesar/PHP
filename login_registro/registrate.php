60b5ff285d5fa6ac6006b7f2e500c60802b3284e
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2022-10-07 06:25:50 -0300

    Add files via upload

--- login_registro/registrate.php
+++ login_registro/registrate.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   $errores = '';
 
-  if (empty($usuario) or empty($password) or empty($password)) {
+  if (empty($usuario) or empty($password) or empty($password2)) {
     $errores .= '<li>Rellene los datos correctamente</li>';
   } else {
     try {
@@ -31,19 +31,40 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // cuando el nombre de usuario NO exista en la base de datos
     // arroja otro resultado cuando el nombre de usuario exista en la base de datos.
 
-    if ($resultado !== false) {
+    if ($resultado != false) {
       $errores .= 'El nombre de usuario ya existe';
-    } else {
-
     }
+
+    // Las siguientes líneas encriptan el password
+    // para dar mayor seguridad.
+    // en nuestro caso vamos a usar el encriptado del encriptado
+    // como contraseña. encriptado doble.
+    $password = hash('sha512', $password);
+    $password = hash('sha512', $password);
+    $password2 = hash('sha512', $password2);
+    $password2 = hash('sha512', $password2);
+
+    if ($password != $password2) {
+      $errores .= 'Las contraseñas no son iguales';
+    }
+
+    //echo "$password <br /> $password2";
+
+  }
+
+  if ($errores == '') {
+    $statement = $conexion->prepare("INSERT INTO usuarios VALUES(null, :usuario, :pass)");
+    $statement->execute(array(
+      ':usuario' => $usuario,
+      ':pass' => $password,
+    ));
+    // Una ve que el usuario se registró lo enviamos a la página
+    // para iniciar sesión
+    header('Location: login.php');
   }
 
   //echo "$usuario - $password - $password2";  
 }
-
-
-
-
 
 require 'views/registrate.view.php';
 
